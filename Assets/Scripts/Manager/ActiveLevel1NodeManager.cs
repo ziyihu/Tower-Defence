d9c0f1552674d73b0de02ea840df2d23edb6f7c4
ziyi hu <huziyi1991@gmail.com>
2016-04-21 21:59:35 -0400

    14th commit by Ziyi Hu

---
+++ Assets/Scripts/Manager/ActiveLevel1NodeManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveLevel1NodeManager : MonoBehaviour {
+
+	public 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
